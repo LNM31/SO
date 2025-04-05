f7d88063fbd01ea4fa8ad5cb3eed67bdf4bf4ebb
LNM31 <licanemes31@gmail.com>
2025-04-05 22:30:27 +0300

    initiate

--- week6/treasure_manager.c
+++ week6/treasure_manager.c
@@ -1,12 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<fcntl.h>
+#include <stdio.h>      // pentru printf, scanf, perror
+#include <stdlib.h>     // pentru exit, malloc (dacă vei folosi)
+#include <string.h>     // pentru strcpy, strcmp, fgets, strcspn
+#include <fcntl.h>      // open, O_CREAT, O_WRONLY etc.
+#include <unistd.h>     // close, read, write, lseek, unlink, rmdir
+#include <sys/stat.h>   // mkdir, stat, lstat, struct stat
+#include <dirent.h>     // manipularea directoarelor (dacă vei citi conținutul)
+#include <time.h>       // pentru afișarea timpului ultimei modificări (cu stat)
 
 typedef struct {
     int id;
     char name[32];
     float x,y;
-    char text[128];
+    char clue[128];
     int value;
 }treasure;
 int main()
