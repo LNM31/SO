8f2113120cb043440ace8b000ab004050c36b069
LNM31 <licanemes31@gmail.com>
2025-04-05 23:45:49 +0300

    creare director

--- week6/treasure_manager.c
+++ week6/treasure_manager.c
@@ -14,8 +14,48 @@ typedef struct {
     char clue[128];
     int value;
 }treasure;
-int main()
+
+void add(char *hunt_id)
+{
+    char dir_path[256],file_path[256],log_path[256],link_path[256];
+    sprintf(dir_path,"%s",hunt_id);
+    sprintf(file_path,"%s/treasures.dat",hunt_id);
+    sprintf(log_path,"%s/logged_hunt",hunt_id);
+    sprintf(link_path,"logged_hunt-%s",hunt_id);
+
+    //Creaza directorul daca nu exista
+    struct stat st;
+    if(stat(dir_path,&st)==-1) 
+    {
+        if(mkdir(dir_path,0777)==-1)
+        {
+            perror("Eroare mkdir");
+            exit(-1);
+        }
+    }
+
+
+
+}
+int main(int argc,char **argv)
 {
     printf("Hello world!\n");
+    if(argc==3)
+    {
+        if(strcmp(argv[1],"add")==0)
+        {
+            add(argv[2]);
+        }
+        else
+        {
+            perror("Argument invalid");
+            exit(-2);
+        }
+    }
+    else
+    {
+        perror("Argumente insuficiente");
+        exit(-3);
+    }
     return 0;
 }
