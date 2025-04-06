b1821c00dfd0f852c90f39c87d43a8e4bfde388e
LNM31 <licanemes31@gmail.com>
2025-04-06 10:57:52 +0300

    Finalizare operatie ADD

--- week6/treasure_manager.c
+++ week6/treasure_manager.c
@@ -15,9 +15,9 @@ typedef struct {
     int value;
 }treasure;
 
-int safe_read_line(char *buffer, size_t size)
+ssize_t safe_read_line(char *buffer, size_t size)
 {
-    int len = read(0, buffer, size - 1); // -1 ca să păstrăm loc pentru \0
+    ssize_t len = read(0, buffer, size - 1); // -1 ca să păstrăm loc pentru \0
     if (len > 0) 
     {
         if (buffer[len - 1] == '\n') 
@@ -35,6 +35,25 @@ int safe_read_line(char *buffer, size_t size)
         buffer[0] = '\0'; // sir gol
     }
     return len;
+}
+
+void log_operation(const char * log_path,const char * what_to_write)
+{
+    int log_fd=open(log_path,O_WRONLY | O_APPEND | O_CREAT, 0777);
+    if(log_fd!=-1)
+    {
+        write(log_fd,what_to_write,strlen(what_to_write));
+        if(close(log_fd)==-1)
+        {
+            perror("eroare log_hunt");
+            exit(-2);
+        }
+    }
+    else
+    {
+        perror("Error open log_hunt");
+        exit(-2);
+    }
 }
 
 void add(char *hunt_id)
@@ -84,8 +103,47 @@ void add(char *hunt_id)
     safe_read_line(buffer,sizeof(buffer));
     t.value=strtol(buffer,NULL,10);
 
-    printf("%d,%s,%f,%f,%s,%d;\n",t.id,t.name,t.x,t.y,t.clue,t.value);
+    //printf("%d,%s,%f,%f,%s,%d;\n",t.id,t.name,t.x,t.y,t.clue,t.value);
 
+    //3.scriere in fisier
+    int fd=open(file_path,O_WRONLY | O_APPEND | O_CREAT, 0777);
+    if(fd==-1)
+    {
+        perror("Eroare fisier treasure.dat");
+        exit(-2);
+    }
+
+    //4. Scriere comoara in fisier binar
+    ssize_t bytes=write(fd,&t,sizeof(treasure));
+    if(bytes!=sizeof(treasure))
+    {
+        perror("Eroare write");
+        close(fd);
+        exit(-4);
+    }
+
+    if(close(fd)==-1)
+    {
+        perror("Eroare close");
+        exit(-4);
+    }
+
+    //5. Scriere in logged_hunt
+    char log[512];
+    sprintf(log,"ADD: ID=%d Name_User=%s Latitude=%.2f Longitude=%.2f Clue=\"%s\" Value=%d\n",t.id,t.name,t.x,t.y,t.clue,t.value);
+    log_operation(log_path,log);
+
+    //6. creare symlimk in caz ca nu exista deja
+    if(access(link_path,F_OK)==-1)
+    {
+        if(symlink(log_path,link_path)==-1)
+        {
+            perror("eroare symlink");
+            exit(-1);
+        }
+    }
+
+    write(1,"✅ Comoara a fost adaugata cu succes!\n",39);
 }
 int main(int argc,char **argv)
 {
